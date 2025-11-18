f9d11235aa74ef86c6fc30218e7bdb08343de814
Pedro Henrique Candido Nascimento <91481238+PH-CN@users.noreply.github.com>
2025-11-18 23:43:37 +0000

    Primeira parte

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+main() {
+    char estado1, estado2;
+    char codigo1[3], codigo2[3];
+    char nome1[30], nome2[30];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int turisticos1, turisticos2;
+
+    printf("Digite uma letra de A a H para a primeira carta, representando um dos 8 estados disponíveis\n");
+    scanf(" %c", &estado1);
+
+    printf("Carta 1:\n");
+    printf("Estado: %c\n",estado1);
+}
